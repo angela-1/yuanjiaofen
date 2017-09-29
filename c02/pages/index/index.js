ed72454a0f51752dd9056b325bc58f9ca45d7be2
Angela-1 <ruoshui_engr@163.com>
2017-09-29 11:12:07 +0800

    add move remove.

--- c02/pages/index/index.js
+++ c02/pages/index/index.js
@@ -13,7 +13,8 @@ Page({
     resultList: [],
     imeoutHandler: 0,
     animationData: {},
-    animation1: {}
+    animation1: {},
+    startX: {}
   },
   //事件处理函数
   bindAddToList() {
@@ -151,5 +152,79 @@ Page({
         animation1: animation.export()
       })
     }.bind(this), 50)
+  },
+
+  //手指刚放到屏幕触发
+  touchS: function (e) {
+
+    console.log("touchS" + e.touches[0].clientX);
+    console.log(this.data.resultList);
+    //判断是否只有一个触摸点
+    if (e.touches.length == 1) {
+      this.setData({
+        //记录触摸起始位置的X坐标
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+  //触摸时触发，手指在屏幕上每移动一次，触发一次
+  touchM: function (e) {
+    console.log("touchM:" + e);
+    var that = this
+    if (e.touches.length == 1) {
+      //记录触摸点位置的X坐标
+      var moveX = e.touches[0].clientX;
+      //计算手指起始点的X坐标与当前触摸点的X坐标的差值
+      var disX = that.data.startX - moveX;
+      //delBtnWidth 为右侧按钮区域的宽度
+      let delBtnWidth = 100
+      var txtStyle = "";
+      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+        txtStyle = "left:0px";
+      } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+        txtStyle = "left:-" + disX + "px";
+        console.log('dd')
+      }
+      //获取手指触摸的是哪一个item
+      var index = e.currentTarget.dataset.index;
+      var list = that.data.resultList;
+
+      //将拼接好的样式设置到当前item中
+      list[index].txtStyle = txtStyle;
+      console.log('kanind', index, list)
+
+      //更新列表的状态
+      this.setData({
+        resultList: list
+      });
+    }
+  },
+  touchE: function (e) {
+    console.log("touchE" + e);
+    var that = this
+    if (e.changedTouches.length == 1) {
+      //手指移动结束后触摸点位置的X坐标
+      var endX = e.changedTouches[0].clientX;
+      //触摸开始与结束，手指移动的距离
+      var disX = that.data.startX - endX;
+
+      var delBtnWidth = that.data.delBtnWidth;
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+     
+      var txtStyle = disX > 100 ? "left:-" + disX + "px" : "left:0px";
+     
+      console.log('disx', disX, txtStyle)
+      // let txtStyle = ''
+      console.log('me')
+      //获取手指触摸的是哪一项
+      var index = e.currentTarget.dataset.index;
+      var list = that.data.resultList;
+      list[index].txtStyle = txtStyle;
+      list.splice(index, 1)
+      //更新列表的状态
+      that.setData({
+        resultList: list
+      });
+    }
   }
 })
